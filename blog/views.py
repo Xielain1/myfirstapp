80a37a962584819ee6894c74156d4a6c97b240d0
Xielain1 <rotmir.klimov@mail.ru>
2020-09-04 23:00:50 +0300

    3

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -9,5 +9,9 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
